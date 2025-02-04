2ad3cb4717080d7749478b2b24f34df0c2fbe48a
Mik TINIGO <tinigojeanmikcomlan@gmail.com>
2025-02-04 17:47:40 +0100

    responsive terminée

---
+++ main.js
@@ -0,0 +1,26 @@
+
+//recuperons les zones de saisies
+let cardNumber = document.getElementById("ipt_2")
+let cardHolderName = document.getElementById("ipt_1")
+let expDateMonth = document.getElementById("ipt_3a")
+let expDateYear = document.getElementById("ipt_3b")
+let cvc = document.getElementById("ipt_4")
+
+//le bouton confirmé
+let btn = document.getElementById("valid")
+
+//Recupérons les zones de sorties
+let cardNumberOutput = document.getElementById("rlt_1")
+let cardHolderNameOutput = document.getElementById("rlt_2")
+let expDateOutput = document.getElementById("rlt_3")
+let cvcOutput = document.getElementById("rlt_4");
+
+btn.onclick = () =>{
+
+}
+
+/* for(i in nom){
+    if(!(nom[i] >= 'a' && nom[i] <= 'z' || nom[i] >= 'A' && nom[i] <= 'Z')
+}
+
+*/
