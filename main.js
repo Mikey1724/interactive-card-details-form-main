854a2d2eaaa1156a30315ee11f9fa6b1edd2cb08
Mik TINIGO <tinigojeanmikcomlan@gmail.com>
2025-02-16 07:00:27 +0100

    fin de la pertie javascript et mises à jour du css et du html

--- main.js
+++ main.js
@@ -1,26 +1,109 @@
 
-//recuperons les zones de saisies
 let cardNumber = document.getElementById("ipt_2")
 let cardHolderName = document.getElementById("ipt_1")
 let expDateMonth = document.getElementById("ipt_3a")
 let expDateYear = document.getElementById("ipt_3b")
 let cvc = document.getElementById("ipt_4")
 
-//le bouton confirmé
-let btn = document.getElementById("valid")
+let btn = document.getElementById("form")
 
-//Recupérons les zones de sorties
+let validBloc = document.getElementById("content_2")
+let currentBloc = document.getElementById("sct_2")
+
 let cardNumberOutput = document.getElementById("rlt_1")
 let cardHolderNameOutput = document.getElementById("rlt_2")
 let expDateOutput = document.getElementById("rlt_3")
 let cvcOutput = document.getElementById("rlt_4");
 
-btn.onclick = () =>{
+
+btn.addEventListener('submit', e =>{
+    e.preventDefault();
+    verify_form();
+})
+
+function verify_form(){
+    const cardNumberValue = cardNumber.value.trim();
+    const cardHolderNameValue = cardHolderName.value.trim();
+    const expDateMonthValue = expDateMonth.value.trim();
+    const expDateYearValue = expDateYear.value.trim();
+    const cvcValue = cvc.value.trim();
+
+    let validName = false;
+    let validNumber = false;
+    let validMonth = false;
+    let validYear = false;
+    let validCVC = false;
+
+    
+    
+    if(cardHolderNameValue == ""){
+        let message = "Can't be blank";
+        afficher_erreur(cardHolderName, message);
+    }else if(!cardHolderNameValue.match(/^[a-zA-Z]+ [a-zA-Z]+$/)){
+        let message = "Wrong format, letters only";
+        afficher_erreur(cardHolderName, message);
+    }else{
+        validName = true;
+    }
+
+    if(cardNumberValue == ""){
+        let message = "Can't be blank";
+        afficher_erreur(cardNumber, message);
+    }else if (!cardNumberValue.match(/^[0-9]{4,4}\ [0-9]{4,4}\ [0-9]{4,4}\ [0-9]{4,4}$/)){
+        let message = "Wrong format, number only";
+        afficher_erreur(cardNumber, message);
+    }else{
+        validNumber = true;
+    }
+
+    if(expDateMonthValue == ""){
+        let message = "Can't be blank";
+        afficher_erreur(expDateMonth, message);
+    }else if(!expDateMonthValue.match(/^[0-9]{2,2}$/) || !(expDateMonthValue >= 1 && expDateMonthValue <= 12)){
+        let message = "Wrong format";
+        afficher_erreur(expDateMonth, message);
+    }else{
+        validMonth = true;
+    }
+
+    if(expDateYearValue == ""){
+        let message = "Can't be blank";
+        afficher_erreur(expDateYear, message);
+    }else if(!expDateYearValue.match(/^[0-9]{2,2}$/) || !(expDateYearValue >= 70 || expDateYearValue <= 26)){
+        let message = "Wrong format";
+        afficher_erreur(expDateYear, message);
+    }else{
+        validYear = true;
+    }
+
+    if(cvcValue == ""){
+        let message = "Can't be blank";
+        afficher_erreur(cvc, message);
+    }else if(!cvcValue.match(/^[0-9]{3,3}$/)){
+        let message = "Wrong format";
+        afficher_erreur(cvc, message);
+    }else{
+        validCVC = true;
+    }
+
+    if(validName == true && validNumber == true && validMonth == true && validYear == true && validCVC == true){
+        currentBloc.style.display = "none";
+        validBloc.style.display = "block";  
+        cardHolderNameOutput.innerText = cardHolderNameValue.toUpperCase();
+        cardNumberOutput.innerText = cardNumberValue;
+        expDateOutput.innerText = `${expDateMonthValue}/${expDateYearValue}`
+        cvcOutput.innerText = cvcValue;
+    }
 
 }
 
-/* for(i in nom){
-    if(!(nom[i] >= 'a' && nom[i] <= 'z' || nom[i] >= 'A' && nom[i] <= 'Z')
-}
 
-*/
+function afficher_erreur(blocName, message){
+    const blocControl = blocName.parentElement;
+    const blocControl2 = blocControl.parentElement;
+    const small = blocControl2.querySelector('small');
+
+    small.innerText = message;
+    blocControl.className = "wrapper error";
+    blocControl2.className = "wrapper2 error";
+}
